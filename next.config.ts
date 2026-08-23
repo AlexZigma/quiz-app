58fcebb64e1e74528865abec12afc782963dde0f
Alex <tor.trapeczia@gmail.com>
2026-08-24 00:14:53 +0300

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
+const baseUrl = process.env.NEXT_PUBLIC_SERVER_URL;
+
 const nextConfig: NextConfig = {
   /* config options here */
+  logging: {
+    fetches: { fullUrl: true },
+    browserToTerminal: true,
+    incomingRequests: true,
+  },
+  trailingSlash: true,
+  rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `https://interns-test-fe.snp.agency/api/:path*/`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
