9fdb994aa4d4baa1321473d8701a547806e66073
Alex <tor.trapeczia@gmail.com>
2026-08-30 23:36:22 +0300

    refactor EditPage

--- src/app/(editor)/tests/[id]/edit/page.tsx
+++ src/app/(editor)/tests/[id]/edit/page.tsx
@@ -2,13 +2,11 @@
 
 import { fetchTestById } from "@/app/api/utils";
 import TestForm from "@/components/commons/Tests/TestForm";
-import { useRequest } from "@/lib/hooks";
-import { Test } from "@/models/test/types";
 import { useTest } from "@/providers/TestProvider";
 import { notFound } from "next/navigation";
-import { use, useCallback, useEffect } from "react";
+import { use, useEffect as useLayoutEffect, useState } from "react";
 
-export default function TestPage({
+export default function TestEditPage({
   params,
 }: {
   params: Promise<{ id: string }>;
@@ -16,19 +14,19 @@ export default function TestPage({
   const { id } = use(params);
 
   const { loadTest } = useTest();
+  const [isLoading, setIsLoaging] = useState(true);
+  const [error, setError] = useState("");
 
-  const { data: test, isLoading } = useRequest<Test>(
-    useCallback(() => fetchTestById(id), [id]),
-  );
-
-  useEffect(() => {
-    if (!test) return;
-    loadTest(test);
-  }, [test, loadTest]);
+  useLayoutEffect(() => {
+    fetchTestById(id)
+      .then((test) => loadTest(test))
+      .catch(() => setError("network error"))
+      .finally(() => setIsLoaging(false));
+  }, [id, loadTest]);
 
   if (isLoading) return;
 
-  if (!test) notFound();
+  if (error) notFound();
 
   return (
     <main>
