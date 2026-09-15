e3ac0236353d6314b1aa9175dad72ce27fbecff0
alex <tor.trapeczia@gmail.com>
2026-09-15 16:08:19 +0300

    add utils: deepEqual function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -22,6 +22,23 @@ export function isAnswerRight(
   );
 }
 
+export function isDeepEqual(a: any, b: any) {
+  if (a === b) return true;
+  if (a == null || b == null || typeof a !== "object" || typeof b !== "object")
+    return false;
+
+  const keysA = Object.keys(a);
+  const keysB = Object.keys(b);
+  if (keysA.length !== keysB.length) return false;
+
+  for (const key of keysA) {
+    if (!keysB.includes(key)) return false;
+    if (!isDeepEqual(a[key], b[key])) return false;
+  }
+
+  return true;
+}
+
 export const mapAnswer = (raw: RawAnswer): Answer => {
   return {
     id: raw.uuid,
